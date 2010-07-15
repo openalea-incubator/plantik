84a6492b3e2088bfc2981285e13892ad6002c3f6
Thomas Cokelaer <cokelaer@gmail.com>
2010-07-15 16:18:43 +0000

    update
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/plantik@9283 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/plantik/tools/mtgtools.py
+++ src/plantik/tools/mtgtools.py
@@ -36,14 +36,26 @@ def get_trunk_branch_radius(g):
     length = [g.property('Branch')[x].radius   for x in indices]
     return length
 
+def get_branch_metamer_number_on_trunk(g):
+    """ get branches on the trunk and returns number of metamer/internode
+    in each of these branches"""
+    length = [len([y for y in Components(x, Scale=4) if Class(y)=='I']) for x in Sons(get_trunk_index(g), EdgeType='+')]
+    return length
 
 def plot(g):
     import pylab
     pylab.hist(get_trunk_branch_length(g))
+    pylab.title('Branch length histogram on the trunk')
     pylab.show()
     pylab.figure(2)
     pylab.hist(get_trunk_branch_radius(g))
+    pylab.title('Branch radius histogram on the trunk')
     pylab.show()
+    pylab.figure(3)
+    pylab.hist(get_branch_metamer_number_on_trunk(g))
+    pylab.title('Metamer number histogram (all branches on the trunk)')
+    pylab.show()
+
 
 
 def create_sequence1(g):
@@ -52,3 +64,5 @@ def create_sequence1(g):
     return Sequences(var1)
 
 
+
+#Plot(v1, 1,6, "Gaussian", ViewPoint="SegmentProfile")
