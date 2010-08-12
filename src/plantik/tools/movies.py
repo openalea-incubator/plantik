af7dff4e710712567139dcd13bdcafe38c74c8f5
Thomas Cokelaer <cokelaer@gmail.com>
2010-08-12 15:12:20 +0000

    move tools/movie from stocatree to movies
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/plantik@9418 ab253cce-29fb-0310-bb2f-979600cdbdeb

---
+++ src/plantik/tools/movies.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+#-*- coding: utf-8 -*-
+"""
+.. topic:: movies summary
+
+    Functionalities to create movies.
+
+    :Code status: mature
+    :Documentation status: mature
+    :Author: Thomas Cokelaer <Thomas.Cokelaer@sophia.inria.fr>
+    :References:
+    :Revision: $Id: fruit.py 8635 2010-04-14 08:48:47Z cokelaer $
+"""
+
+import platform
+import glob
+import os
+
+
+def create_movie(input_glob='*.png', output_filename='output', format='avi', width=800, height=600, fps=24, type='png'):
+    """generate a movie in avi format from a list of PNG images using mencode
+
+    :param input_glob: a glob to a list of files (default is ``*.png``)
+    :param output_filename: the output filename
+    :param format: the movie format (default is avi)
+    :param width: width of the movie frame (default is 800)
+    :param height: height of the movie frame (default is 600)
+    :param fps: frame per second (Default is 24)
+    :param format: the input format (type is png)
+
+    :platform: linux
+
+    .. note:: the linux command is
+
+        mencoder mf://@/tmp/file.txt -mf w=800:h=600:fps24:type=png -ovc lavc -lavcopts vcodec=msmpeg4:mbd=2:trell -oac copy -o output.avi
+
+    >>> create_movie('test*.png', output_filename='movie1', format='avi', fps=20)
+
+    .. todo:: for all platforms
+    .. todo:: provide more options
+    """
+    files = glob.glob(input_glob)
+    files.sort()
+    if 'linux' in platform.platform().lower():
+        tempfilename = '/tmp/list.txt'
+        tempfile = open(tempfilename, 'w')
+        for file in files:
+            tempfile.write(file+'\n')
+        tempfile.close()
+
+        size = '-mf w=%s:h=%s:fps=%s:type=%s' % (width, height, fps, type)
+        cmd = 'mencoder mf://@%s %s -ovc lavc -lavcopts vcodec=msmpeg4:mbd=2:trell -oac copy -o %s' % (tempfilename, size, output_filename + '.' + format)
+        status = os.system(cmd)
+        if status!=0:
+            print '!! command failed. check that mencoder is installed of files exists. (%s)' % cmd
+    else:
+        raise NotImplementedError('create_movie is only available for linux platforms at the moment.')
+        #os.system('rm %s' % tempfilename)
