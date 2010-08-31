1e2e5fcaf64d4888977095cff5ecec9e37a6bd67
Thomas Cokelaer <cokelaer@gmail.com>
2010-08-31 15:15:49 +0000

    cleanup; may not be used anymore. complete doc and tests
    git-svn-id: https://scm.gforge.inria.fr/authscm/pradal/svn/vplants/vplants/trunk/plantik@9476 ab253cce-29fb-0310-bb2f-979600cdbdeb

--- src/plantik/biotik/root.py
+++ src/plantik/biotik/root.py
@@ -4,65 +4,32 @@ from openalea.plantik.biotik.component import ComponentInterface
 
 class Root(ComponentInterface):
     """Root class
-    
+
     Specialised version of ComponentInterface dedicated to Root.
-    
+
     This root class is an abstract root component that is used
-    to store global information such as total resource of a plant. 
-    
-    
+    to store global information such as total resource of a plant.
+
+
     """
-    def __init__(self, initial_resource, birthdate=None, 
-                 id=None, min_radius=0.001):
+    def __init__(self, initial_resource, birthdate=None):
         """
-        
-        :param initial_resource: the amount of initial resource/biomass of 
-            the root
-        :param label: default is 'Root'
-        :param birthdate:
-        :param id:
-        
-        :attributes:
-            * those inherited by SimulationInterface: :attr:`age`, 
-              :attr:`demand`, :attr:`birthdate`, ...
-            * :attr:`radius`
-            * :attr:`R` the global resource available in the root system
+
         """
         ComponentInterface.__init__(self, label='Root', birthdate=birthdate, id=id)
-        
-        self._radius = min_radius
         self._R = initial_resource
 
-    
-    def _getRadius(self):
-        return self._radius
-    def _setRadius(self, value):
-        self._radius = value
-    radius = property(_getRadius, _setRadius, None, "Radius's Docstring")
-        
-        
     def _getR(self):
         return self._R
-    def _setR(self, R):
-        self._R = R
+    def _setR(self, value):
+        self._R = value
     R = property(_getR, _setR, None, doc="returns global resource.")
-   
-    def update(self, dt):
-        """Update the root characteristics
-        
-        :param dt: in days
-        """
-        super(Root, self).update(dt)
-        self._radius *= 1.0
 
-        # if hypothesis is to reset the root ressource.
-        
-    def demand_calculation(self, **kargs):
-        len(kargs)
-        self.demand = 0
+    def update(self, dt):
+        super(Root, self).update(dt)
+
+    def demandCalculation(self):
         return self.demand
-    def resource_calculation(self, **kargs):
-        len(kargs)
-        self.resource = 0
+    def resourceCalculation(self):
         return self.resource
 
--- test/test_root.py
+++ test/test_root.py
@@ -8,11 +8,8 @@ def test_root():
     root.update(1)
     assert root.R == 2
     assert root.age.days == 1
-    assert root.radius == 0.001
-    root.radius *= 2.
-    assert root.radius == 0.002
     root.R += 1
 
 
-    root.demand_calculation()
-    root.resource_calculation()
+    root.demandCalculation()
+    root.resourceCalculation()
